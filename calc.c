3cdbb211fabadc18417a4df7daec6279f4135d05
yoongunwo <glove2275@pusan.ac.kr>
2022-05-11 09:51:11 +0900

    add sub function

--- calc.c
+++ calc.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int add(int a, int b);
-
+int sub(int a, int b);
 int main() {
+	char c;
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %d\n", a, b, add(a, b));
+	scanf("%c %d %d",&c, &a, &b);
+	switch(c){
+		case '+' : printf("%d", add(a, b)); break;
+		case '-' : printf("%d", sub(a, b)); break;
+	}
+
 }
 
