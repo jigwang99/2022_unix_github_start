3a318b68693f8dbfc9d824c10efd15567bec2b7d
seunghun <juniper0917@gmail.com>
2022-05-11 09:49:52 +0900

    secondcalccommit

--- calc.c
+++ calc.c
@@ -3,7 +3,7 @@ long long mul(int a, int b);
 
 int main() {
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %lld\n", a, b, mul(a, b));
+	scanf("* %d %d", &a, &b);
+	printf("%lld\n",mul(a, b));
 }
 
